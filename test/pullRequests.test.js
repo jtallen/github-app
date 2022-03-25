e56061088868e041362328eab80d3fde8ce33f6f
turner <jturnerallen@gmail.com>
2022-03-24 17:28:10 -0700

    converted tests to async/await format

--- test/pullRequests.test.js
+++ test/pullRequests.test.js
@@ -2,23 +2,17 @@ const pullRequests = require('../src/getPullRequests');
 const constants = require('../constants/data');
 
 // tests for getPullRequests
-test('Number of open pull requests should be 2 for turner-site repo', (done) => {
-  pullRequests.getPullRequests(constants.pullsUrl).then((output) => {
-    expect(output.length).toBe(2);
-    done();
-  });
+test('Number of open pull requests should be 2 for turner-site repo', async () => {
+  const output = await pullRequests.getPullRequests(constants.pullsUrl);
+  expect(output.length).toBe(2);
 });
 
-test('Author of 2nd open pull request should be jtallen turner-site repo', (done) => {
-  pullRequests.getPullRequests(constants.pullsUrl).then((output) => {
-    expect(output[1].author).toBe('jtallen');
-    done();
-  });
+test('Author of 2nd open pull request should be jtallen turner-site repo', async () => {
+  const output = await pullRequests.getPullRequests(constants.pullsUrl);
+  expect(output[1].author).toBe('jtallen');
 });
 
-test('Number of commits for 2nd open pull request should be', (done) => {
-  pullRequests.getPullRequests(constants.pullsUrl).then((output) => {
-    expect(output[0].commit_count).toBe(4);
-    done();
-  });
+test('Number of commits for 2nd open pull request in turner-site repo should be', async () => {
+  const output = await pullRequests.getPullRequests(constants.pullsUrl);
+  expect(output[1].commit_count).toBe(4);
 });
